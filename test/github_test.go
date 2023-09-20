c0442922f7009681a81c1f9d68e8c82f23b3886f
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2023-09-20 22:09:05 +0900

    update test

--- test/github_test.go
+++ test/github_test.go
@@ -3,18 +3,25 @@ package scrapings
 import (
 	"log"
 	"testing"
+	"time"
 
 	"local.packages/scrapings"
 )
 
 func Test_01(t *testing.T) {
 	if result, err := scrapings.GetGitHubLatestReleaseDateByUserAndRep("yt-dlp/yt-dlp"); err != nil {
-		log.Println(err)
+		t.Errorf(err.Error())
 	} else {
 		log.Println(result)
+		if parsedTime, err := time.Parse("2006-01-02T15:04:05Z07:00", result); err != nil {
+			t.Errorf(err.Error())
+		} else {
+			log.Println(parsedTime)
+			log.Println(parsedTime.Unix())
+		}
 	}
 	if result, err := scrapings.GetGitHubLatestReleaseDate("yt-dlp", "yt-dlp"); err != nil {
-		log.Println(err)
+		t.Errorf(err.Error())
 	} else {
 		log.Println(result)
 	}
